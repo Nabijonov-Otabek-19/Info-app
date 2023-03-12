357a81b72bcd9b411d41952bce154203bff0bc25
DAVID <nabijonovo23@gmail.com>
2023-03-12 09:02:12 +0300

    project updated

---
+++ app/src/main/java/uz/gita/infoapp/InfoActivity.java
@@ -0,0 +1,83 @@
+package uz.gita.infoapp;
+
+import android.content.Intent;
+import android.os.Bundle;
+import android.widget.ImageView;
+import android.widget.TextView;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+public class InfoActivity extends AppCompatActivity {
+
+    private ImageView image;
+    private TextView title, desc;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_info);
+
+        Intent intent = getIntent();
+        int number = intent.getIntExtra("number", 0);
+        loadData(number);
+    }
+
+    private void loadData(int number) {
+        image = findViewById(R.id.img_animal);
+        title = findViewById(R.id.txt_animalName);
+        desc = findViewById(R.id.txt_about);
+
+        if (number == 1) {
+            image.setImageResource(R.drawable.cat);
+            title.setText("CAT");
+            desc.setText(R.string.cat);
+
+        } else if (number == 2) {
+
+            image.setImageResource(R.drawable.dog);
+            title.setText("dog");
+            desc.setText(R.string.dog);
+
+        } else if (number == 3) {
+
+            image.setImageResource(R.drawable.lion);
+            title.setText("lion");
+            desc.setText(R.string.lion);
+
+        } else if (number == 4) {
+            image.setImageResource(R.drawable.begemot);
+            title.setText("hypopotamus");
+            desc.setText(R.string.begemot);
+
+        } else if (number == 5) {
+            image.setImageResource(R.drawable.kangaroo);
+            title.setText("kangaroo");
+            desc.setText(R.string.kangaroo);
+
+        } else if (number == 6) {
+            image.setImageResource(R.drawable.zebra);
+            title.setText("zebra");
+            desc.setText(R.string.zebra);
+
+        } else if (number == 7) {
+            image.setImageResource(R.drawable.jiraf);
+            title.setText("giraffe");
+            desc.setText(R.string.jiraf);
+
+        } else if (number == 8) {
+            image.setImageResource(R.drawable.wolf);
+            title.setText("wolf");
+            desc.setText(R.string.wolf);
+
+        } else if (number == 9) {
+            image.setImageResource(R.drawable.fox);
+            title.setText("fox");
+            desc.setText(R.string.fox);
+
+        } else {
+            image.setImageResource(R.drawable.elephant);
+            title.setText("elephant");
+            desc.setText(R.string.elephant);
+        }
+    }
+}
---
+++ app/src/main/java/uz/gita/infoapp/MainActivity.java
@@ -0,0 +1,34 @@
+package uz.gita.infoapp;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.annotation.SuppressLint;
+import android.content.Intent;
+import android.os.Bundle;
+
+public class MainActivity extends AppCompatActivity {
+
+    @SuppressLint("MissingInflatedId")
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+        findViewById(R.id.cat).setOnClickListener(view -> openWindow(1));
+        findViewById(R.id.dog).setOnClickListener(view -> openWindow(2));
+        findViewById(R.id.lion).setOnClickListener(view -> openWindow(3));
+        findViewById(R.id.begemot).setOnClickListener(view -> openWindow(4));
+        findViewById(R.id.kangaroo).setOnClickListener(view -> openWindow(5));
+        findViewById(R.id.zebra).setOnClickListener(view -> openWindow(6));
+        findViewById(R.id.jiraf).setOnClickListener(view -> openWindow(7));
+        findViewById(R.id.wolf).setOnClickListener(view -> openWindow(8));
+        findViewById(R.id.fox).setOnClickListener(view -> openWindow(9));
+        findViewById(R.id.elephant).setOnClickListener(view -> openWindow(10));
+    }
+
+    private void openWindow(int num) {
+        Intent intent = new Intent(MainActivity.this, InfoActivity.class);
+        intent.putExtra("number", num);
+        startActivity(intent);
+    }
+}
